e02b743446b123301b7c8d125e140f8adb8b9a26
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-04-07 17:04:24 +0200

    Added ConfirmTagHelper

---
+++ Altairis.TagHelpers/ConfirmTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Altairis.TagHelpers {
+    [HtmlTargetElement("a", Attributes = "href")]
+    [HtmlTargetElement("button")]
+    [HtmlTargetElement("input", Attributes = "[type=submit]")]
+    [HtmlTargetElement("input", Attributes = "[type=button]")]
+    public class ConfirmTagHelper : TagHelper {
+
+        public string ConfirmMessage { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            base.Process(context, output);
+
+            if (!string.IsNullOrWhiteSpace(this.ConfirmMessage)) {
+                output.Attributes.Add("onclick", $"return window.confirm('{this.ConfirmMessage}');");
+            }
+        }
+    }
+}
