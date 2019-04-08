880327d69100ea01f89d2d16526448931f0b9d4f
Michal Altair Valášek <github@rider.cz>
2019-04-08 12:56:04 +0200

    Implemented CheckboxListTagHelper

---
+++ Altairis.TagHelpers.DemoApp/Pages/CheckBoxList.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Altairis.TagHelpers.DemoApp.Pages {
+    public class CheckBoxListModel : PageModel {
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new InputModel();
+
+        public class InputModel {
+
+            public ICollection<string> SelectedValues { get; set; } = new HashSet<string>();
+
+        }
+
+        public string Message { get; set; }
+
+        public IEnumerable<SelectListItem> ListValues => new List<SelectListItem>(new[] {
+            new SelectListItem("Item 1", "1", this.Input.SelectedValues.Contains("1")),
+            new SelectListItem("Item 2", "2", this.Input.SelectedValues.Contains("2")) { Disabled = true },
+            new SelectListItem("Item 3", "3", this.Input.SelectedValues.Contains("3")),
+        });
+
+        public void OnPost() {
+            this.Message = "Selected item IDs: " + string.Join(", ", this.Input.SelectedValues);
+        }
+    }
+}
---
+++ Altairis.TagHelpers/CheckboxListTagHelper.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altairis.TagHelpers {
+    public class CheckboxListTagHelper : TagHelper {
+
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        [HtmlAttributeName("asp-items")]
+        public IEnumerable<SelectListItem> Items { get; set; }
+
+        public string Class { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            base.Process(context, output);
+
+            output.TagName = "ul";
+            if (!string.IsNullOrWhiteSpace(this.Class)) output.Attributes.Add("class", this.Class);
+
+            var items = new List<SelectListItem>(this.Items);
+            for (var i = 0; i < items.Count; i++) {
+                var fieldName = this.For.Name;
+                var fieldId = $"{this.For.Name.Replace('.', '_')}[{i}]";
+
+                // Create checkbox
+                var input = new TagBuilder("input") {
+                    TagRenderMode = TagRenderMode.SelfClosing
+                };
+                input.Attributes.Add("type", "checkbox");
+                input.Attributes.Add("name", fieldName);
+                input.Attributes.Add("id", fieldId);
+                input.Attributes.Add("value", items[i].Value);
+                if (items[i].Selected) input.Attributes.Add("checked", "checked");
+                if (items[i].Disabled) input.Attributes.Add("disabled", "disabled");
+
+                // Create label
+                var label = new TagBuilder("label");
+                label.Attributes.Add("for", fieldId);
+                label.InnerHtml.Append(items[i].Text);
+
+                // Create list item with contents
+                var li = new TagBuilder("li");
+                li.InnerHtml.AppendHtml(input);
+                li.InnerHtml.AppendHtml(label);
+                output.Content.AppendHtml(li);
+            }
+        }
+    }
+}
