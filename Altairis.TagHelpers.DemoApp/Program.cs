f57f01600f769afeaae6ea590af0f5d7e4916294
Michal Altair Valášek <github@rider.cz>
2019-11-14 15:30:10 +0100

    Using Endpoint routing in .NET Core 3.0

--- Altairis.TagHelpers.DemoApp/Program.cs
+++ Altairis.TagHelpers.DemoApp/Program.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Altairis.TagHelpers.DemoApp {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- Altairis.TagHelpers.DemoApp/Startup.cs
+++ Altairis.TagHelpers.DemoApp/Startup.cs
@@ -1,22 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Altairis.TagHelpers.DemoApp {
     public class Startup {
         public void ConfigureServices(IServiceCollection services) {
             services.AddHttpContextAccessor();
-            services.AddMvc();
+            services.AddRazorPages();
             //services.Configure<TimeTagHelperOptions>(options => {
             //    options.GeneralDateFormatter = d => string.Format("{0:d}", d);
             //});
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseMvc();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
