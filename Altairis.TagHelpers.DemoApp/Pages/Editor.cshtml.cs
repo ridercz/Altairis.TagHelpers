82aa964382ae870da1279bf94e7c93375dae96fe
Michal Altair Valasek <michal.valasek@altairis.cz>
2022-07-10 02:14:30 +0200

    Add EditorTagHelper

---
+++ Altairis.TagHelpers.DemoApp/Pages/Editor.cshtml.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Altairis.TagHelpers.DemoApp.Pages {
+    public class EditorModel : PageModel {
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new InputModel();
+
+        public class InputModel {
+
+            [Display(Name = "Name")]
+            public string FullName { get; set; }
+
+            [Display(Name = "E-mail"), EmailAddress]
+            public string EmailAddress { get; set; }
+
+            [Display(Name = "Comment"), DataType(DataType.MultilineText)]
+            public string Comment { get; set; }
+
+        }
+
+    }
+}
---
+++ Altairis.TagHelpers/EditorTagHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace Altairis.TagHelpers;
+
+[HtmlTargetElement("editor", Attributes = "asp-for")]
+public class EditorTagHelper : TagHelper {
+    private readonly IHtmlHelper htmlHelper;
+
+    public EditorTagHelper(IHtmlHelper htmlHelper) {
+        this.htmlHelper = htmlHelper ?? throw new ArgumentNullException(nameof(htmlHelper));
+    }
+
+    [HtmlAttributeName("asp-for")]
+    public ModelExpression For { get; set; }
+
+    public string TemplateName { get; set; }
+
+    [ViewContext, HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        base.Process(context, output);
+        var awareHtmlHelper = (IViewContextAware)this.htmlHelper;
+        awareHtmlHelper.Contextualize(this.ViewContext);
+        output.TagName = null;
+        output.Content.SetHtmlContent(this.htmlHelper.Editor(this.For.Name, this.TemplateName));
+    }
+
+}
