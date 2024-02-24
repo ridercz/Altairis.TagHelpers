67e9d432ee3a946e0e2d5c231c9c7467cef06bed
Michal Altair Valášek <github@rider.cz>
2024-02-24 23:02:51 +0100

    Add ConditionalAttributeTagHelper.

---
+++ Altairis.TagHelpers/ConditionalAttributeTagHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Altairis.TagHelpers;
+
+[HtmlTargetElement("*", Attributes = "conditional-attribute-name,conditional-attribute-state")]
+public class ConditionalAttributeTagHelper : TagHelper {
+
+    public string ConditionalAttributeName { get; set; } = string.Empty;
+
+    public string? ConditionalAttributeValue { get; set; }
+
+    public bool ConditionalAttributeState { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        if (this.ConditionalAttributeState) output.Attributes.SetAttribute(this.ConditionalAttributeName, this.ConditionalAttributeValue ?? this.ConditionalAttributeName);
+    }
+
+}
